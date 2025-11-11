74c9b1b8bbf7f172b3ea434b683525d3a3951c93
Alomgir Hossen <alomgir.dev@gmail.com>
2025-11-12 02:05:03 +0600

    Add Vercel Web Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
-import Footer from '../components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Footer />
+      <Analytics />
     </>
   )
 }
