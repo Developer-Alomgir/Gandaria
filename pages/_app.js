3a090ea0d217112f151e8a30cef76f47f18d821a
Alomgir.exe <alomgirsojol0005@gmail.com>
2025-11-12 02:08:34 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
-import { Analytics } from '@vercel/analytics/react'   
+import Footer from '../components/Footer'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />  
+      <Footer />
     </>
   )
 }
