d5e410701a00b62517806a83b1f575f63481e82f
Alomgir.exe <alomgirsojol0005@gmail.com>
2025-11-15 22:31:14 +0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import Footer from '../components/Footer'
+import type { AppProps } from 'next/app'   // ✅ Import AppProps
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
